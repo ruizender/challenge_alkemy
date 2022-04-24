b370237c4258fb2389d31fb4b558f86c9a718971
Ender Ruiz <ruiz.valera@gmail.com>
2022-04-24 18:03:01 -0400

    delete enviroment variable from model user

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,9 @@
+# class UserNotifierMailer < ApplicationMailer
+#   default :from => 'ruiz.valera@gmail.com'
+
+#   def send_signup_email(user)
+#     @user = user
+#     mail( :to => @user.email,
+#     :subject => 'Gracias por registrarte, bienvenid@' )
+#   end
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,23 @@
 class User < ApplicationRecord
   has_secure_password
-
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
                     presence: true,
                     uniqueness: true
-
-                    require 'sendgrid-ruby'
-                    include SendGrid
-                
-                    def send_email
-                        user_email = self.email
-                
-                        from = Email.new(email: 'ruiz.valera@gmail.com')
-                        to = Email.new(email: user_email)
-                        subject = "Successful registration - Disney API"
-                        content = Content.new(type: 'text/plain', value: "Welcome to our disney characters API,\n\nThanks for registering")
-                        mail = Mail.new(from, subject, to, content)    
-                        sg = SendGrid::API.new(api_key: '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')
-                        response = sg.client.mail._('send').post(request_body: mail.to_json)
-                        puts response.status_code
-                        puts response.body
-                        puts response.headers
-                    end
-
-
-
-
+                    
+  require 'sendgrid-ruby'
+  include SendGrid
+  def send_email
+    user_email = self.email
+    from = Email.new(email: 'ruiz.valera@gmail.com')
+    to = Email.new(email: user_email)
+    subject = "Successful registration - Disney API"
+    content = Content.new(type: 'text/plain', value: "Welcome to our disney characters API,\n\nThanks for registering")
+    mail = Mail.new(from, subject, to, content)
+    key = ENV['SENDGRID_API_KEY']
+    sg = SendGrid::API.new(api_key: "#{key}")
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+  end
 end
